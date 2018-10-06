de70ca1b58315a01c1322c8fc531ba207a239d96
Nail <cobratms@gmail.com>
2018-10-06 22:39:16 +0300

    Ready!

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,21 @@ public class Main {
 
         toLeftFirstVersion();
         toLeftSecondVersion();
+
+        reverseArray();
+    }
+
+    private static void reverseArray() {
+        int[] toReverse = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+
+        for(int i = 0; i < toReverse.length / 2; i++) {
+            toReverse[i] = toReverse[i] ^ toReverse[toReverse.length - (i + 1)];
+            toReverse[toReverse.length - (i + 1)] = toReverse[i] ^ toReverse[toReverse.length - (i + 1)];
+            toReverse[i] = toReverse[i] ^ toReverse[toReverse.length - (i + 1)];
+
+
+        }
+        showArray(toReverse);
     }
 
     private static void toLeftSecondVersion() {
@@ -64,4 +79,12 @@ public class Main {
             System.out.println();
         }
     }
+
+    private static void showArray(int[] arr) {
+        System.out.println();
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
 }
